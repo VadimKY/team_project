4edf8d8db15ab5f1e33bcc30a385c824a1a5a25d
VadimKY <vykolerov@mail.ru>
2023-02-09 17:17:59 +0300

    Изменен index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './components/App/App';
-import AppAnt from "./AppAntHome/AppAnt/AppAnt";
-//import AppMui from "./AppMui/AppMui"
+//import AppAnt from "./AppAntHome/AppAnt/AppAnt";
+import AppMui from "./AppMui/AppMui"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //root.render(<App />);
-root.render(<AppAnt />);
-//root.render(<AppMui />);
+//root.render(<AppAnt />);
+root.render(<AppMui />);
