5ea6d6cdc6c7bd8f464b0a58590485e2227b7c25
VadimKY <vykolerov@mail.ru>
2023-02-12 18:04:28 +0300

    ÐРазработал PostList

---
+++ src/PostList/PostList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Grid from '@mui/material/Grid';
+import Post from "../Post/Post";
+
+
+const PostList = ({posts}) => {
+    return <div>
+        <Grid container spacing="40px">
+            {posts.map(item => (
+                <Post key={item._id} {...item} />
+            ) )}
+            {/*<Post key={posts[0]._id} {...posts[0]} />*/}
+        </Grid>
+
+    </div>
+}
+
+export default PostList;
