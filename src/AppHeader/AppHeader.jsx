b2f89a969819edeb77cf22e64d00a36019c0c387
VadimKY <vykolerov@mail.ru>
2023-02-17 23:11:55 +0300

    Поправил AppHeader

--- src/AppHeader/AppHeader.jsx
+++ src/AppHeader/AppHeader.jsx
@@ -3,9 +3,12 @@ import { AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material';
 import {AccountBalanceSharp, Menu} from '@mui/icons-material';
 import { grey } from '@mui/material/colors';
 import s from "./AppHeader.module.css";
+import ColorButtons from "../Button/Button";
+import SimpleDialogDemo from "../Modal/Modal";
 
 
 const AppHeader = () => {
+
     return (
         <AppBar position="static">
 
@@ -21,8 +24,9 @@ const AppHeader = () => {
                     aria-label="menu"
                     sx={{ mr: 2 }}>
                     <Menu sx={{color:  grey[900]}} />
-
                 </IconButton>
+                <ColorButtons />
+                <SimpleDialogDemo />
             </Toolbar>
         </AppBar>
     )
